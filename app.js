60be1399f93a8872c5c8aaf5e937a86eb14d017d
Roshan <shahroshan0720@gmail.com>
2023-06-15 22:14:19 +0530

    setup app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require('dotenv').config;
+require('dotenv').config();
 require('express-async-errors');
 
 const express = require('express');
@@ -7,15 +7,15 @@ const app = express();
 
 const mainRouter = require('./routes/main');
 const notFoundMiddleware = require('./middleware/not-found');
-//const errorHandlerMiddleware = require('./middleware/error-handler');
+const errorHandlerMiddleware = require('./middleware/error-handler');
 
-
+app.use(express.static('./public'));
 app.use(express.json());
 
 app.use('/api/v1',mainRouter);
 
 app.use(notFoundMiddleware);
-//app.use(errorHandlerMiddleware);
+app.use(errorHandlerMiddleware);
 
 const port = process.env.Port || 3000;
 
